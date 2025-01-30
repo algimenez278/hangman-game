8fd4cbc7f4e6d0ec583b6e0b06125856f1739894
Aluna <algimenez@fi.uba.ar>
2025-01-30 14:05:19 -0300

    new

---
+++ src/main/java/org/example/HangmanController.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HangmanController extends Application {
+    private HangmanModel model;
+    private HangmanView view;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        model = new HangmanModel();
+        view = new HangmanView(primaryStage);
+
+        view.setWordDisplay(model.getWordDisplay());
+        view.setAttemptsLeft(model.getAttemptsLeft());
+        view.setIncorrectGuesses(model.getIncorrectGuesses());
+
+        view.setGuessButtonAction(this::handleGuess);
+        view.drawHangman(model.getAttemptsLeft());
+    }
+
+    private void handleGuess() {
+        String input = view.getInput();
+        if (input.length() == 1) {
+            char guessedLetter = input.charAt(0);
+            model.makeGuess(guessedLetter);
+            view.clearInput();
+            view.setWordDisplay(model.getWordDisplay());
+            view.setAttemptsLeft(model.getAttemptsLeft());
+            view.setIncorrectGuesses(model.getIncorrectGuesses());
+            view.drawHangman(model.getAttemptsLeft());
+            checkGameStatus();
+        }
+    }
+
+    private void checkGameStatus() {
+        if (model.isGameOver()) {
+            String message = model.getAttemptsLeft() <= 0 ? "¡Perdiste! La palabra era: " + model.getSelectedWord() : "¡Ganaste! La palabra era: " + model.getSelectedWord();
+            view.showGameOver(message);
+        }
+    }
+}
